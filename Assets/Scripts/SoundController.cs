16546e9bf8a049500af6cf429bb1e8f6b551eb65
nahmnenic <rytpbarbos@mail.ru>
2026-01-27 18:29:04 +0300

    Some sound

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -1,11 +1,20 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    public string soundName;
+    [SerializeField] private FMODUnity.StudioEventEmitter _fmod;
+    public float Delay;
 
     public void PlaySound()
     {
-        
+        StartCoroutine(PlaySoundWithDelay());
+    }
+
+    private IEnumerator PlaySoundWithDelay()
+    {
+        yield return new WaitForSeconds(Delay);
+        _fmod.Play();
     }
 }
