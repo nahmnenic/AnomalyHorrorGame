1a59c750af431a87095ff733f0c322304af72380
nahmnenic <rytpbarbos@mail.ru>
2026-05-09 22:16:48 +0300

    Set settings and screenshot room update

--- Assets/Scripts/UI/SettingWindow.cs
+++ Assets/Scripts/UI/SettingWindow.cs
@@ -1,4 +1,5 @@
 using System;
+using Player;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,8 @@ namespace UI
         
         [SerializeField] private Text _sensTxt;
         [SerializeField] private Slider _sensValue;
+        
+        private CameraController _cameraController;
 
         private void Start()
         {
@@ -20,11 +23,15 @@ namespace UI
         public void ChangeVolume()
         {
             _volumeTxt.text = _volumeValue.value.ToString();
+            string vcaPath = "vca:/Master";
+            FMOD.Studio.VCA vca = FMODUnity.RuntimeManager.GetVCA(vcaPath);
+            vca.setVolume(_volumeValue.value / 100);
         }
         
         public void ChangeSensetivity()
         {
             _sensTxt.text = Math.Round(_sensValue.value, 2).ToString();
+            FindFirstObjectByType<CameraController>().mouseSense = _sensValue.value / 10;
         }
     }
 }
