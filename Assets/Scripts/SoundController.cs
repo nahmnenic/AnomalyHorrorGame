9b05bebcc6bad3a4a719d634283ac467148e8802
nahmnenic <rytpbarbos@mail.ru>
2026-06-22 21:33:24 +0300

    Light update

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -7,7 +7,10 @@ public class SoundController : MonoBehaviour
     [SerializeField] private FMODUnity.StudioEventEmitter _fmod;
     public float Delay;
 
+    public bool _isPlaying = false;
+    
     public bool Disposable;
+    public bool Light;
 
     [ContextMenu("PlaySound")]
     public void PlaySound()
@@ -19,6 +22,7 @@ public class SoundController : MonoBehaviour
     public void StopSound()
     {
         _fmod.Stop();
+        _isPlaying = false;
     }
 
     public bool IsPlaying()
@@ -30,14 +34,28 @@ public class SoundController : MonoBehaviour
     {
         yield return new WaitForSeconds(Delay);
         _fmod.Play();
+        _isPlaying = true;
         if (Disposable)
         {
             _fmod = null;
+            _isPlaying = false;
         }
     }
     
     private void OnDestroy()
     {
         if(_fmod != null) _fmod.Stop();
+    }
+    
+    private void OnEnable()
+    {
+        if (!Light || _isPlaying) return;
+        PlaySound();
+    }
+
+    private void OnDisable()
+    {
+        if (!Light) return;
+        StopSound();
     }
 }
